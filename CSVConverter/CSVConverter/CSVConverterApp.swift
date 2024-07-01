af489ab58db86366f79800c743904fa0082bdd02
seunggyun-jeong <jskyun98@gmail.com>
2024-07-01 15:47:05 +0900

    [Init] Project Init

---
+++ CSVConverter/CSVConverter/CSVConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSVConverterApp.swift
+//  CSVConverter
+//
+//  Created by Gyunni on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CSVConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
